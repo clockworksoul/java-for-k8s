22b5bbb346bd3396210d274cc98abed9aee17150
Matthew Titmus <matthew.titmus@gmail.com>
2017-12-06 15:49:21 -0500

    Automate this

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ rm -Rf /tmp/openjdk
 
 git clone https://github.com/docker-library/openjdk.git /tmp/openjdk
 
-for v in $(find /tmp/openjdk | grep 'j../Dockerfile' | sed 's|/Dockerfile||g' | sed 's|/tmp/openjdk/||g'); do
+for v in $(find /tmp/openjdk | grep 'Dockerfile' | sed 's|/Dockerfile||g' | sed 's|/tmp/openjdk/||g' | sed 's|/|-|g'); do
 	rm -Rf $v
 	mkdir $v
 	cp dynamic-memory-opts $v/
@@ -18,6 +18,15 @@ ADD entrypoint.sh /tmp/entrypoint.sh
 
 ENTRYPOINT ["/tmp/entrypoint.sh"]
 EOT
+
+	cd $v
+	docker pull openjdk:$v
+	docker build -t clockworksoul/java-for-k8s:$v .
+	docker push clockworksoul/java-for-k8s:$v
+
+	docker rmi clockworksoul/java-for-k8s:$v
+	docker rmi openjdk:$v
+	cd ..
 done
 
 rm -Rf /tmp/openjdk
